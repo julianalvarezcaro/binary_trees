e82926122832d4baa63db624928a61c9cbff5cc5
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2020-11-26 20:18:13 +0000

    Trying solutions

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,8 +16,15 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	if (tree->left)
 		b_left += 1 + binary_tree_balance(tree->left);
+	else
+	{
+		b_left = 0;	
+	}
 	if (tree->right)
 		b_right += 1 + binary_tree_balance(tree->right);
-
-	return (b_right - b_left);
+	else
+	{
+		b_right = 0;
+	}
+	return (b_left - b_right);
 }
